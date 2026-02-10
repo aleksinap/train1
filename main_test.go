a4ce42454be976adf9fc119f4955fdf0b5a71c86
Алексей Пилюгин <aleksinap@yandex.ru>
2026-02-10 14:47:49 +0300

    change main_test.go

--- main_test.go
+++ main_test.go
@@ -1,25 +1,12 @@
 package main
 
-import (
-	"os"
-	"testing"
-)
+import "testing"
 
+// Пример теста функции MaxInt
 func TestMaxInt(t *testing.T) {
 	a, b := 2, 7
 	res := MaxInt(a, b)
 	if res != b {
 		t.Errorf("expected %d, got %d", b, res)
 	}
-}
-
-// TestMain используется для подготовки/очистки перед и после тестов
-func TestMain(m *testing.M) {
-	// setup code (если нужно)
-
-	code := m.Run() // запускаем все тесты
-
-	// teardown code (если нужно)
-
-	os.Exit(code)
 }
