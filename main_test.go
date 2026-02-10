23ea1cf0930607881bf389a39e712417258699ee
Алексей Пилюгин <aleksinap@yandex.ru>
2026-02-10 14:36:39 +0300

    change main_test.go

--- main_test.go
+++ main_test.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
 func TestMaxInt(t *testing.T) {
 	a, b := 2, 7
-
 	res := MaxInt(a, b)
-
 	if res != b {
 		t.Errorf("expected %d, got %d", b, res)
 	}
 }
 
 func TestMain(m *testing.M) {
-	main()
+	// можно добавить setup, если нужно
+
+	code := m.Run() // запускаем все тесты
+
+	// можно добавить teardown, если нужно
+
+	os.Exit(code)
 }
